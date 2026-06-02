bb230a23a2eda51844748167ad2e3aa385db60d0
alexj0319jr@gmail.com <alexj0319jr@gmail.com>
2026-06-01 17:48:07 -0700

    fix: add route and nav for bayes theorem

--- cipherquest/src/components/Navbar.jsx
+++ cipherquest/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ function Navbar() {
 
       <div className="nav-links">
         <NavLink to="/lessons">Lessons</NavLink>
+        <NavLink to="/bayes-theorem">Bayes</NavLink>
         <NavLink to="/perfect-secrecy">Perfect Secrecy</NavLink>
         <NavLink to="/attack-simulator">Attack Simulator</NavLink>
         <NavLink to="/practice">Practice</NavLink>
