7a9b6ca0bdd20a47d10599e80a1fdc36e2f8666e
alexj0319jr@gmail.com <alexj0319jr@gmail.com>
2026-06-01 17:49:32 -0700

    fix: styling lesson cards clickable

--- cipherquest/src/App.jsx
+++ cipherquest/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 
 import Home from "./pages/Home";
 import Lessons from "./pages/Lessons";
+import BayesTheorem from "./pages/BayesTheorem";
 import PerfectSecrecy from "./pages/PerfectSecrecy";
 import AttackSimulator from "./pages/AttackSimulator";
 import Practice from "./pages/Practice";
@@ -16,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/lessons" element={<Lessons />} />
+          <Route path="/bayes-theorem" element={<BayesTheorem />} />
           <Route path="/perfect-secrecy" element={<PerfectSecrecy />} />
           <Route path="/attack-simulator" element={<AttackSimulator />} />
           <Route path="/practice" element={<Practice />} />
