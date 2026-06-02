d7f1222ec050970e2ea6d3a9a4ffdd1a0441e3f2
alexj0319jr@gmail.com <alexj0319jr@gmail.com>
2026-06-01 17:11:54 -0700

    feat: add navigation bar

---
+++ cipherquest/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link, NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <Link to="/" className="logo">
+        CipherQuest
+      </Link>
+
+      <div className="nav-links">
+        <NavLink to="/lessons">Lessons</NavLink>
+        <NavLink to="/perfect-secrecy">Perfect Secrecy</NavLink>
+        <NavLink to="/attack-simulator">Attack Simulator</NavLink>
+        <NavLink to="/practice">Practice</NavLink>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
